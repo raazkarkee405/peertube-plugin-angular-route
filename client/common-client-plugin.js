3a0bc048fb0a1b63937e8b71fd9dd22df8ced9cc
Durgaraj Karki <durgaraj726@gmail.com>
2021-12-10 11:57:16 +0545

    added route

--- client/common-client-plugin.js
+++ client/common-client-plugin.js
@@ -1,7 +1,64 @@
-function register ({ registerHook, peertubeHelpers }) {
-  console.log('Hello world')
+function register({ registerHook, peertubeHelpers }) {
+
+ // to display content in the route /plugins/information
+  registerHook({
+    target: "action:router.navigation-end",
+    handler: async ({ path }) => {
+      if (path === "/my-information") {
+        await fetch(peertubeHelpers.getBaseRouterRoute() + "/mydata", {
+          method: "GET",
+          headers: peertubeHelpers.getAuthHeader(),
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            console.log("HEELO::", data.email);
+            const mainRow = document.getElementsByClassName("main-row")[0];
+            const notFound =
+              document.getElementsByTagName("my-page-not-found")[0];
+
+            mainRow.removeChild(notFound);
+
+            const element = document.createElement("div");
+            element.setAttribute("id", "new-value");
+
+            const h1 = document.createElement("h1");
+
+            const newContext = document.createTextNode(data.email);
+
+            h1.appendChild(newContext);
+
+            element.appendChild(h1);
+
+            mainRow.appendChild(element);
+          });
+      } else {
+        const deleteNode = document.getElementById("new-value");
+        deleteNode.parentElement.removeChild(deleteNode);
+      }
+    },
+  });
+
+  // to add item in left menu
+  registerHook({
+    target: "filter:left-menu.links.create.result",
+    handler: (items) => {
+      return [
+        ...items,
+        {
+          key: "features",
+          title: "Additional Features",
+          links: [
+            {
+              label: "My Information",
+              icon: "trending",
+              shortLabel: "My Information",
+              path: "/my-information",
+            },
+          ],
+        },
+      ];
+    },
+  });
 }
 
-export {
-  register
-}
+export { register };
--- main.js
+++ main.js
@@ -1,20 +1,28 @@
-async function register ({
-  registerHook,
-  registerSetting,
-  settingsManager,
-  storageManager,
-  videoCategoryManager,
-  videoLicenceManager,
-  videoLanguageManager
-}) {
+async function register({ peertubeHelpers, registerHook, getRouter }) {
+  const logger = peertubeHelpers.logger;
+  const router = getRouter();
 
+  router.get("/mydata", async (req, res) => {
+
+    const username = 'root'
+    const results = await peertubeHelpers.database.query(
+      'SELECT "email" from "user" WHERE "username" = $username',
+      {
+        type: 'SELECT',
+        bind: { username }
+      }
+    )
+
+    logger.info('root email is ' + results[0]['email'])
+    return res.json({ email: results[0]['email'] });
+  });
 }
 
-async function unregister () {
-  return
+async function unregister() {
+  return;
 }
 
 module.exports = {
   register,
-  unregister
-}
+  unregister,
+};
